6c26547fd00ea69b11f68d8ed6cdf91da7e0f4b8
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2021-01-18 14:15:40 +0500

    Default setup

---
+++ src/Layout/Router.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Router extends Component {
+
+    render() {
+        return (
+            <div>
+                Hamza
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <App />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
